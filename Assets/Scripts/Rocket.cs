acba2e16f225f625cbbb9045a4b06c6520dbd946
emark1 <devel010@umn.edu>
2020-01-07 15:55:29 -0600

    Tweaked controls. Added tags and obstacles

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
 
+    [SerializeField] float rotateThrust = 10f;
+    [SerializeField] float verticalThrust = 10f;
+
     Rigidbody rigidbody;
     AudioSource rocketAudio;
     // Start is called before the first frame update
@@ -17,23 +20,46 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput() {
+    private void Thrust() {
         if (Input.GetKey(KeyCode.Space)) {
-            rigidbody.AddRelativeForce(Vector3.up);
+            float thrustSpeed = verticalThrust * Time.deltaTime;
+            rigidbody.AddRelativeForce(Vector3.up * verticalThrust);
             if (!rocketAudio.isPlaying) {
                 rocketAudio.Play();
             } 
         } else {
             rocketAudio.Stop();
         }
+    }
+
+    private void Rotate() {
+        rigidbody.freezeRotation = true;
+        float rotationSpeed = rotateThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.D)) {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationSpeed);
         } else if (Input.GetKey(KeyCode.A)) {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationSpeed);
+        }
+
+        rigidbody.freezeRotation = false;
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        switch (collision.gameObject.tag) {
+            case "Friendly":
+                Debug.Log("Meh");
+                break;
+            case "Fuel": 
+                Debug.Log("BIG FUEL");
+                break;
+            default: 
+                Debug.Log("DEAD");
+                break;
         }
     }
 
