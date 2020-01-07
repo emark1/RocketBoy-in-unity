2e6991116e2de02c8e56b85afcaa79a09ad61525
emark1 <devel010@umn.edu>
2020-01-06 21:56:32 -0600

    Added rocket, rocket thrust

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+
+    Rigidbody rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessInput();
     }
+
+    private void ProcessInput() {
+        if (Input.GetKey(KeyCode.Space)) {
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            Debug.Log("TURNING RIGHT");
+        } else if (Input.GetKey(KeyCode.A)) {
+            Debug.Log("TURNING LEFT");
+        }
+    }
+
 }
