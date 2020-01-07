d6a780ba40253810ae90d2987c202b6683e355cc
emark1 <devel010@umn.edu>
2020-01-07 14:34:54 -0600

    Rocket audio added

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -6,10 +6,12 @@ public class Rocket : MonoBehaviour
 {
 
     Rigidbody rigidbody;
+    AudioSource rocketAudio;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        rocketAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -21,12 +23,17 @@ public class Rocket : MonoBehaviour
     private void ProcessInput() {
         if (Input.GetKey(KeyCode.Space)) {
             rigidbody.AddRelativeForce(Vector3.up);
+            if (!rocketAudio.isPlaying) {
+                rocketAudio.Play();
+            } 
+        } else {
+            rocketAudio.Stop();
         }
 
         if (Input.GetKey(KeyCode.D)) {
-            Debug.Log("TURNING RIGHT");
+            transform.Rotate(-Vector3.forward);
         } else if (Input.GetKey(KeyCode.A)) {
-            Debug.Log("TURNING LEFT");
+            transform.Rotate(Vector3.forward);
         }
     }
 
